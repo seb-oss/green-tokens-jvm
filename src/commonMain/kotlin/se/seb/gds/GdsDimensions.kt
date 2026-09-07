6ee0aea36c37db8957bc738fd7d3e262984d0506
Fredrik Eneroth <fredrik.eneroth@seb.se>
2026-09-07 09:37:38 +0200

    Add GdsDimensions and bump version name to 3.1.7

---
+++ src/commonMain/kotlin/se/seb/gds/GdsDimensions.kt
@@ -0,0 +1,46 @@
+
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+
+
+package se.seb.gds
+
+enum class GdsDimensions {
+    Space0,
+    Space5Xs,
+    Space4Xs,
+    Space3Xs,
+    Space2Xs,
+    SpaceXs,
+    SpaceS,
+    SpaceM,
+    SpaceL,
+    SpaceXl,
+    Space2Xl,
+    Space3Xl,
+    Space4Xl,
+    Space5Xl,
+    Space6Xl,
+    Space7Xl,
+    Space8Xl,
+    Space9Xl,
+    SpaceMax,
+    RadiusO,
+    RadiusNone,
+    Radius5Xs,
+    Radius4Xs,
+    Radius3Xs,
+    Radius2Xs,
+    RadiusXs,
+    RadiusS,
+    RadiusM,
+    RadiusL,
+    RadiusXl,
+    Radius2Xl,
+    Radius3Xl,
+    Radius4Xl,
+    Radius5Xl,
+    RadiusMax,
+}
